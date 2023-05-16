79002bc828c3e95dec59832cda079158fa7dd926
JuanJo18B <juanjo18b@udenar.edu.co>
2023-05-16 17:54:46 -0500

    registro 2.0

--- BD/connectDB.php
+++ BD/connectDB.php
@@ -1,3 +1,12 @@
 <?php
-    
+    $Chost = 'localhost';
+    $Cnombre = 'root';
+    $Ccontraseña = '';
+    $Cdb = 'restaurante';
+
+    $con = new mysqli($Chost,$Cnombre,$Ccontraseña,$Cdb);
+
+    if ($con->connect_errno){
+        die("Ha ocurrido un error");
+    }
 ?>
