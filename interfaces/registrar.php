a83da9d910634b525140ae079006fe16a60884d4
JuanJo18B <juanjo18b@udenar.edu.co>
2023-05-16 15:52:14 -0500

    cambio en las rutas css

--- interfaces/registrar.php
+++ interfaces/registrar.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="/css/style.css">
-	<link rel="stylesheet" href="/css/style_login.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/style_login.css">
 	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
 	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Registrarse</title>
