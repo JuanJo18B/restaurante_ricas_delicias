c784b197393eb2decd55957c5b1c1c4063c19e0b
JuanJo18B <juanjo18b@udenar.edu.co>
2023-05-17 13:46:11 -0500

    ortografia

--- interfaces/gerente/form_registrar_cli.php
+++ interfaces/gerente/form_registrar_cli.php
@@ -96,7 +96,7 @@
 		</div>
 		</div>
 	</footer>
-	<script src="..//js//validar_contra.js"></script>
+	<script src="../../js/validar_contra.js"></script>
 	<script src="https://kit.fontawesome.com/71b9e775fc.js" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
