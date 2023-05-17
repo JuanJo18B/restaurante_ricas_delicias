c3454e37e2ad68d29c62ab5b18af358687c0581e
JuanJo18B <juanjo18b@udenar.edu.co>
2023-05-16 19:29:33 -0500

    perfil gerente

---
+++ BD/login.php
@@ -0,0 +1,19 @@
+<?php
+    require '..//BD//connectDB.php';
+    session_start();
+    echo("estas dentro");
+    // INICIO DE SESION GERENTE //
+    if(isset($_POST['nombre_user']) && isset($_POST['contraseña']) && isset($_POST['codigo'])){
+        $usuario_ger = $_POST['nombre_user'];
+        $contraseña  = $_POST['contraseña'];
+        $codigo_ger  = $_POST['codigo'];
+        $sql         = "SELECT COUNT(*) as contar FROM gerente WHERE codigo = 'codigo' and contraseña = 'contraseña'";
+        $consulta    = mysqli_query($con,$sql);
+        $array       = mysqli_fetch_array($consulta);
+        if ($array['contar']>0){
+            echo("fucniona");
+            $_SESSION['username'] = $usuario_ger;
+            header("location: ../interfaces/gerente/perfil_gerente.php");
+        }
+    }
+?>
