5be1cd3e628eed34ba1119c78fd6da80b098db17
JuanJo18B <juanjo18b@udenar.edu.co>
2023-05-16 21:27:24 -0500

    perfil gerente

--- BD/login.php
+++ BD/login.php
@@ -9,11 +9,16 @@
         $contraseña  = $_POST['contraseña'];
         $codigo_ger  = $_POST['codigo'];
         $sql         = "SELECT COUNT(*) as contar FROM gerente WHERE codigo_ger = '$codigo_ger'";
+        $sql1        = "SELECT * FROM gerente WHERE codigo_ger = '$codigo_ger'";
         $consulta    = mysqli_query($con,$sql);
+        $consulta1    = mysqli_query($con,$sql1);
         $array       = mysqli_fetch_array($consulta);
+        $array1       = mysqli_fetch_array($consulta1);
         if ($array['contar']>0){
             echo("fucniona");
             $_SESSION['username'] = $usuario_ger;
+            $_SESSION['correo'] = $array1['correo'];
+            $_SESSION['codigo'] = $codigo_ger;
             header("location: ../interfaces/gerente/perfil_gerente.php");
         }else{
             echo("no esta funcionando");
--- interfaces/gerente/perfil_gerente.php
+++ interfaces/gerente/perfil_gerente.php
@@ -1,5 +1,62 @@
-<?php
-	session_start();
-	$usuario = $_SESSION['username'];
-	echo "<h1> Bienvenido $usuario </h1>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../../css/style.css">
+	<link rel="stylesheet" href="../../css/style_login.css">
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	<title>Mi perfil</title>
+</head>
+<body>
+	<header>
+		<div class="contenedor">
+			<h2 class="logotipo">Ricas delicias</h2>
+			<nav>
+				<a href="./index.html">Inicio</a>
+				<a href="./menu.html">Menú</a>
+				<a href="./servicios.html">Servicios</a>
+				<a href="./contacto.html">Contacto</a>
+				<a href="./acerca_de.html">Acerca de</a>
+				<a href="#"class="activo">Iniciar sesión</a>
+			</nav>
+		</div>
+	</header>
+	<main>
+		<?php
+		session_start();
+		$usuario = $_SESSION['username'];
+		$correo = $_SESSION['correo'];
+		$codigo = $_SESSION['codigo'];
+		echo "<h1> Bienvenido $usuario con codiigo $codigo y correo $correo que bien</h1>";
+		?>
+	</main>
+	<footer style="background-color: #1b1b1b; padding: 20px;">
+		<div class="footer-container">
+		<div class="footer-section">
+			<br>
+			<ul class="social-icons">
+			<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+			<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+			<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+			<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+			</ul>
+			<br>
+			<ul class="social-foo">
+				<a> Quienes somos</a>
+				<a> Preguntas frecuentes</a>
+				<a> Contacto</a>
+			</ul>
+			<br>
+			<p class="derechos">© 2023 Ricas Delicias. Todos los derechos reservados.</p>
+		</div>
+		</div>
+	</footer>
+	<script src="..//js//validar_contra.js"></script>
+	<script src="https://kit.fontawesome.com/71b9e775fc.js" crossorigin="anonymous"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
+
