9f6f171eb789a7f48833ec9608cdda85a8d5d0af
JuanJo18B <juanjo18b@udenar.edu.co>
2023-05-30 18:04:42 -0500

    edicion

---
+++ interfaces/inventario/vistas/navbar.php
@@ -0,0 +1,38 @@
+<nav role="navigation" aria-label="main navigation">
+    <div id="navbarBasicExample" class="navbar-menu">
+        <h2 class="logotipo">Ricas delicias</h2>
+        <div class="navbar-start">
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">Categorías</a>
+
+                <div class="navbar-dropdown">
+                    <a href="index.php?vista=category_new" class="navbar-item">Nueva</a>
+                    <a href="index.php?vista=category_list" class="navbar-item">Lista</a>
+                    <a href="index.php?vista=category_search" class="navbar-item">Buscar</a>
+                </div>
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">Productos</a>
+
+                <div class="navbar-dropdown">
+                    <a href="index.php?vista=product_new" class="navbar-item">Nuevo</a>
+                    <a href="index.php?vista=product_list" class="navbar-item">Lista</a>
+                    <a href="index.php?vista=product_category" class="navbar-item">Por categoría</a>
+                    <a href="index.php?vista=product_search" class="navbar-item">Buscar</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a href="../../interfaces/gerente/perfil_gerente.php" class="button is-link is-rounded">
+                        Mi perfil
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
